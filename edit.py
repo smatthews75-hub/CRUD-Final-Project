7316b30418c0e8d16446b73980c399a0598a753c
smatthews75-hub <smatthews75@students.calvin.ac.id>
2025-12-01 11:01:26 +0700

    damn I thought ive been in the features branch

--- edit.py
+++ edit.py
@@ -2,18 +2,28 @@
 import csv
 
 input_file = './6000_Largest_Companies_ranked_by_Market_Cap.csv'
-output_file = './6000_Largest_Companies_ranked_by_Market_Cap.csv'
+output_file = './6000_Largest_Companies_ranked_by_Market_Cap_NEW.csv'
 field_to_remove = "Rank"
-
-with open(input_file, newline="", encoding="utf-8") as infile, \
+headers = []
+with open(input_file, "r", newline="", encoding="utf-8") as infile, \
      open(output_file, "w", newline="", encoding="utf-8") as outfile:
-    
     reader = csv.DictReader(infile)
+    print(reader.fieldnames)
     headers = [h for h in reader.fieldnames if h != field_to_remove]
-    
+    print(headers)
     writer = csv.DictWriter(outfile, fieldnames=headers)
     writer.writeheader()
     
     for row in reader:
         row.pop(field_to_remove, None)
         writer.writerow(row)
+
+
+# with open(output_file, "w", newline="", encoding="utf-8") as outfile:
+    
+#     writer = csv.DictWriter(outfile, fieldnames=headers)
+#     writer.writeheader()
+    
+    # for row in reader:
+    #     row.pop(field_to_remove, None)
+    #     writer.writerow(row)
--- main.py
+++ main.py
@@ -16,7 +16,9 @@ if __name__ == '__main__':
     fieldnames, main_data_dict, groups_of_data = df_.cf_.read_n_dict(file_path, GROUPABLES, RANKED_FIELD, STORED_RANKS)
 
     # finish and write evrything in the system back to file storage
-    df_.cf_.write_dicts(file_path, fieldnames, main_data_dict, groups_of_data, RANKED_FIELD, STORED_RANKS)
+    # df_.cf_.write_dicts(file_path, fieldnames, main_data_dict, groups_of_data, RANKED_FIELD, STORED_RANKS)
+    ranking = groups_of_data[RANKED_FIELD].keys().sort()
+    print(ranking)
 
     # for name in main_data_dict:
     #     print(f"{name} : {main_data_dict[name]}")
